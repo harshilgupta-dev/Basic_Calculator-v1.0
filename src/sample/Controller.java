9ac16bef793e401713e03e745f0e7f5cb1069f2b
Harshil Gupta <harshilgupta0099@gmail.com>
2021-01-30 14:39:55 +0530

    add methods

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,7 +5,31 @@ import javafx.scene.control.TextField;
 
 public class Controller {
     public TextField firstNumberTextField,secondNumberTextField,resultTextField;
+    public void calculateAdd(ActionEvent actionEvent) {
+        double first = Double.parseDouble(firstNumberTextField.getText());
+        double second = Double.parseDouble(secondNumberTextField.getText());
+        resultTextField.setText(String.valueOf(first + second));
+    }
 
+    public void calculateSubtract(ActionEvent actionEvent) {
+        double first = Double.parseDouble(firstNumberTextField.getText());
+        double second = Double.parseDouble(secondNumberTextField.getText());
+        resultTextField.setText(String.valueOf(first - second));
+    }
 
+    public void calculateMultiply(ActionEvent actionEvent) {
+        double first = Double.parseDouble(firstNumberTextField.getText());
+        double second = Double.parseDouble(secondNumberTextField.getText());
+        resultTextField.setText(String.valueOf(first * second));
+    }
 
+    public void calculateDivision(ActionEvent actionEvent) {
+        double first = Double.parseDouble(firstNumberTextField.getText());
+        double second = Double.parseDouble(secondNumberTextField.getText());
+        resultTextField.setText(String.valueOf(first / second));
+    }
 }
+
+
+
+
